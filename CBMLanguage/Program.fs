6e7f7b70b23b3f13a7bf4ea8ae171388193fdeb0
Ștefan-Iulian Alecu <165364995+vmaskmovps@users.noreply.github.com>
2024-11-12 19:40:22 +0200

    chore: ...maybe this time around it'll be F#?

--- CBMLanguage/Program.fs
+++ CBMLanguage/Program.fs
@@ -1,2 +1,6 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿namespace CBM
+
+module Program =
+    let main args =
+        printfn $"Arguments passed to function : %A{args}"
+        0
